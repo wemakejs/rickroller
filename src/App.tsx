d9e2488e4acbd320a0f1c18336a7d85a18754803
Bill Feng <hello@billfeng.io>
2021-04-04 15:17:42 +0900

    Add dynamic meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
-import React, { useEffect } from "react";
+import firebase from "firebase/app";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Helmet } from "react-helmet";
+
 import "./App.css";
 
 export const App = () => {
+  const { slug } = useParams<{ slug: string }>();
+  const [meta, setMeta] = useState({ title: "", description: "", image: "" });
+
+  console.log(slug);
+
   useEffect(() => {
+    if (!slug) return;
+    const fetchData = async () => {
+      const res = await firebase
+        .firestore()
+        .collection("rollers")
+        .where("slug", "==", slug)
+        .get();
+      const { title, description, image } = res.docs[0].data();
+      setMeta({ title, description, image });
+    };
+    fetchData();
+  }, [slug]);
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "development") return;
     setTimeout(() => {
       window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
     }, 500);
@@ -10,6 +34,13 @@ export const App = () => {
 
   return (
     <div className="App">
+      <Helmet>
+        <title>{meta.title}</title>
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:image" content={meta.image} />
+      </Helmet>
+
       <p>Loading...</p>
     </div>
   );
